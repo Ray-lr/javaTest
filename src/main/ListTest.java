51fc3535aef53572fc97ee99ff82822e22290a85
lirui <1027050822@qq.com>
2019-07-16 15:47:42 +0800

    新增列表测试类ListTest 修改了Student类,新增构造器,重写toString方法

---
+++ src/main/ListTest.java
@@ -0,0 +1,35 @@
+package main;
+
+import main.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 类的描述: 列表测试
+ *
+ * @author : lirui
+ * @date : 2019/7/16 15:35
+ */
+public class ListTest {
+
+    /**
+     * 功能描述: 测试结果为,同一个对象第二次放入list时,会覆盖第一次的值
+     *
+     * @param args:
+     * @author : lirui
+     * @date : 2019/7/16 15:45
+     */
+    public static void main(String[] args) {
+        Student student = new Student(1, "wo");
+        List<Student> list = new ArrayList<>(2);
+        list.add(student);
+        System.out.println(list.get(0).toString());
+        student.setId(2);
+        student.setName("ni");
+        list.add(student);
+        for (Student s : list) {
+            System.out.println(s.toString());
+        }
+    }
+}
--- src/main/entity/Student.java
+++ src/main/entity/Student.java
@@ -7,6 +7,14 @@ public class Student {
     private Integer id;
     private String name;
 
+    public Student() {
+    }
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Integer getId() {
         return id;
     }
@@ -21,5 +29,13 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
